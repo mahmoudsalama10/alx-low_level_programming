57e354d3b62645b6425782369ef0e2200fadde4f
mahmoud salama <m.hedia45@gmail.com>
2023-03-17 19:43:27 -0700

    alx tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar(' ')
+			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
