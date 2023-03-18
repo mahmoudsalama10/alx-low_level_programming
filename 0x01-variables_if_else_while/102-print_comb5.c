45054c3ba7c8b411bae53fc291114b89913fce3a
mahmoud salama <m.hedia45@gmail.com>
2023-03-17 19:34:17 -0700

    alx tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,16 +18,17 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar(' ');
+			putchar(' ')
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 != 98 && num2 != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+			
 		}
+		
 	}
 
 	putchar('\n');
